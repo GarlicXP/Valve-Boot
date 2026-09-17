177aac25762ed4b73f491fba0bddcf53ab3713dc
GarlicXP <3376601271@qq.com>
2026-09-17 22:05:29 +0800

    Add files via upload

---
+++ Valve Boot/Resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Valve Boot.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_VALVEBOOT_DIALOG            102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_VALVEBOOT                   107
+#define IDI_SMALL                       108
+#define IDC_VALVEBOOT                   109
+#define IDR_MAINFRAME                   128
+// 内嵌开机视频资源：在 .rc 中注册为 "IDR_RCDATA1 RCDATA \"rcdata1.bin\""（ID = 130）。
+// Valve Boot.cpp 中 #define IDR_VIDEO_STARTUP 130，二者必须一致，否则 FindResource 找不到视频。
+#define IDR_RCDATA1                     130
+#define IDR_VIDEO_STARTUP               130
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
